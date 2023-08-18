550ba9ab0178109377e70e00f10a750435aa5db4
Karasev K. A <karasev.ka@tgmu.ru>
2023-08-18 10:05:15 +1000

    Initial commit

---
+++ Application/Class1.cs
@@ -0,0 +1,24 @@
+﻿namespace Application
+{
+    public abstract class Class1
+    {
+        private int _ = 0;
+
+        public Class1(int a)
+        {
+            _ = a;
+        }
+
+        public int MyMethod()
+        {
+            return _;
+        }
+    }
+
+    public class Class2 : Class1
+    {
+        public Class2(int a) : base(a)
+        {
+        }
+    }
+}
