b189de6b996c6d25856351195a63695770338adf
Karasev K. A <karasev.ka@tgmu.ru>
2023-09-08 17:07:07 +1000

    Добавил в Agenda явно поле AdvertisementId

--- Domain/Entities/Agenda.cs
+++ Domain/Entities/Agenda.cs
@@ -9,6 +9,7 @@ namespace Domain.Entities
     public class Agenda : BaseEntity
     {
         public int Id { get; set; }
+        public int AdvertisementId { get; set; }
         public string Description { get; set; }
         public string Speakers { get; set; }
         public string? CoSpeakers { get; set; }
