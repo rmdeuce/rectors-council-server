5c1e1373f2aaa819fb5a1ef6fa160dfd206bc20c
Karasev K. A <karasev.ka@tgmu.ru>
2023-09-11 12:38:52 +1000

    Добавил ДТОшки для создания и изменения повестки

--- WebAPI/Models/DTO/Agendas/UpdateAgendaDTO.cs
+++ WebAPI/Models/DTO/Agendas/UpdateAgendaDTO.cs
@@ -1,10 +1,8 @@
 ﻿namespace WebAPI.Models.DTO.Agendas
 {
-    public class CreateAgendaDTO
+    public class UpdateAgendaDTO
     {
         public int Id { get; set; }
-        public int? AdvertisementId { get; set; }
-        public int? NewsId { get; set; }
         public string Description { get; set; }
         public string Speakers { get; set; }
         public string? CoSpeakers { get; set; }
