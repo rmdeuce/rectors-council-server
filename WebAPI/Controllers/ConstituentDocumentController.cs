35dfcba5ea08ce396d63ccafea29971c486824f1
rmdeuce <rmdeuce@gmail.com>
2023-09-15 17:29:12 +1000

    создан ConstituteDocumentController

---
+++ WebAPI/Controllers/ConstituentDocumentController.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+
+namespace WebAPI.Controllers
+{
+    public class ConstituentDocumentController : BaseController
+    {
+        private readonly IMapper mapper;
+        private readonly ILogger logger;
+
+        public ConstituentDocumentController(IMapper mapper, ILoggerFactory loggerFactory)
+        {
+            this.mapper = mapper;
+            this.logger = loggerFactory.CreateLogger("Constituent document info");
+
+        }
+
+    }
+}
