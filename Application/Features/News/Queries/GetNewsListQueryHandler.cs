683ec864f82cc6ab6f900ba3a467178373f07727
rmdeuce <rmdeuce@gmail.com>
2023-10-03 12:18:06 +1000

    [RCS-996] реализован GetAll запрос в NewsController, реализованы DTO для News, GetNewsListQuery, GetNewsListQueryHandler

---
+++ Application/Features/News/Queries/GetNewsListQueryHandler.cs
@@ -0,0 +1,40 @@
+﻿using Application.Features.News.Queries.DTO;
+using Application.Interfaces;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Features.News.Queries
+{
+    public class GetNewsListQueryHandler : IRequestHandler<GetNewsListQuery, NewsListDTO>
+    {
+        private readonly IAppDBContext dBContext;
+        private readonly IMapper mapper;
+
+        public GetNewsListQueryHandler(IAppDBContext dBContext, IMapper mapper)
+        {
+            this.dBContext = dBContext;
+            this.mapper = mapper;
+        }
+
+        public async Task<NewsListDTO> Handle(GetNewsListQuery request, CancellationToken cancellationToken)
+        {
+            var query = await dBContext.News
+                .Where(e => !e.IsDeleted)
+                .OrderByDescending(e => e.UpdatedAt)
+                .Skip(request.Position)
+                .Take(request.Take)
+                .ProjectTo<NewsPreviewDTO>(mapper.ConfigurationProvider)
+                .ToListAsync(cancellationToken);
+
+            var total = await dBContext.News.CountAsync(e => !e.IsDeleted);
+
+            return new NewsListDTO
+            {
+                Total = total,
+                News = query
+            };
+        }
+    }
+}
---
+++ WebAPI/Controllers/NewsController.cs
@@ -0,0 +1,39 @@
+﻿using Application.Features.News.Queries;
+using Application.Features.News.Queries.DTO;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+
+    public class NewsController : BaseController
+    {
+        private readonly IMapper mapper;
+        private readonly ILogger logger;
+
+        public NewsController(IMapper mapper, ILoggerFactory loggerFactory)
+        {
+            this.mapper = mapper;
+            this.logger = loggerFactory.CreateLogger("News info");
+        }
+
+        [HttpGet("{page?}/{count?}")]
+        public async Task<ActionResult<NewsListDTO>> GetAll(int page = 0, int count = 10)
+        {
+            page = page > 0 ? page - 1 : 0;
+            count = count > 50 ? 50 : count;
+
+            var position = count * page;
+
+            var query = new GetNewsListQuery
+            {
+                Position = position,
+                Take = count
+            };
+
+            var result = await Mediator.Send(query);
+            return Ok(result);
+        }
+    }
+}
