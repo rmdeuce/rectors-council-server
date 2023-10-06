31715c955f96aecab1a9a9f0dd5188ab03166931
rmdeuce <rmdeuce@gmail.com>
2023-10-06 12:43:05 +1000

    [RCS-1018] удален createAgendaDTO из features/queries/dto, так как не имел никакой роли, аналогичный класс определен в webapi

---
+++ WebAPI/Controllers/CouncilMemberController.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+
+namespace WebAPI.Controllers
+{
+    public class CouncilMemberController : BaseController
+    {
+        private readonly IMapper mapper;
+        private readonly ILogger logger;
+
+        public CouncilMemberController(IMapper mapper, ILoggerFactory loggerFactory)
+        {
+            this.mapper = mapper;
+            this.logger = loggerFactory.CreateLogger("Council member info");
+        }
+
+        //public async Task<ActionResult<>>
+
+    }
+}
