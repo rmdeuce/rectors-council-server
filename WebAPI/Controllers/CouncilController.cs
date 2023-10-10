32a1886312aae6ecbb7663d497ebb5b49b42c4e1
rmdeuce <rmdeuce@gmail.com>
2023-10-10 12:38:10 +1000

    [RCS-1075] Реализован метод Get для CouncilController, добавлен CouncilDTO, GetCouncilQuery, GetCouncilQueryHandler

---
+++ WebAPI/Controllers/CouncilController.cs
@@ -0,0 +1,32 @@
+﻿using Application.Features.Councils.Queries;
+using Application.Features.Councils.Queries.DTO;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    public class CouncilController : BaseController
+    {
+        private readonly IMapper mapper;
+        private readonly ILogger logger;
+
+        public CouncilController(IMapper mapper, ILoggerFactory loggerFactory)
+        {
+            this.mapper = mapper;
+            this.logger = loggerFactory.CreateLogger("Council info");
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CouncilDTO>> Get(int id)
+        {
+            var query = new GetCouncilQuery
+            {
+                Id = id
+            };
+
+            var result = await Mediator.Send(query);
+
+            return Ok(result);
+        }
+    }
+}
