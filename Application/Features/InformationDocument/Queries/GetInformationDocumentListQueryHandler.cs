48c1ea176b7b6ce4d2d2b1e2ed531981f2100f1d
rmdeuce <rmdeuce@gmail.com>
2023-12-11 10:00:03 +1000

    [RCS-1735] Реализован метод GetAll для сущности InformationDocument

---
+++ Application/Features/InformationDocument/Queries/GetInformationDocumentListQueryHandler.cs
@@ -0,0 +1,45 @@
+﻿using Application.Features.InformationDocument.Queries.DTO;
+using Application.Interfaces;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.InformationDocument.Queries
+{
+    public class GetInformationDocumentListQueryHandler : IRequestHandler<GetInformationDocumentListQuery, InformationDocumentListDTO>
+    {
+        private readonly IAppDBContext dbContext;
+        private readonly IMapper mapper;
+
+        public GetInformationDocumentListQueryHandler(IAppDBContext dbContext, IMapper mapper)
+        {
+            this.dbContext = dbContext;
+            this.mapper = mapper;
+        }
+        
+        public async Task<InformationDocumentListDTO> Handle(GetInformationDocumentListQuery request, CancellationToken cancellationToken)
+        {
+            var query = await dbContext.InformationDocuments
+                .Where(e => !e.IsDeleted)
+                .OrderByDescending(e => e.UpdatedAt)
+                .Skip(request.Position)
+                .Take(request.Take)
+                .ProjectTo<InformationDocumentDTO>(mapper.ConfigurationProvider)
+                .ToListAsync(cancellationToken);
+
+            var total = await dbContext.InformationDocuments.CountAsync(e => !e.IsDeleted);
+
+            return new InformationDocumentListDTO
+            {
+                Total = total,
+                InformationDocuments = query
+            };
+        }
+    }
+}
---
+++ WebAPI/Controllers/InformationDocumentController.cs
@@ -0,0 +1,39 @@
+﻿using Application.Features.InformationDocument.Queries;
+using Application.Features.InformationDocument.Queries.DTO;
+using Application.Features.News.Queries.DTO;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    public class InformationDocumentController : BaseController
+    {
+        private readonly IMapper mapper;
+        private readonly ILogger logger;
+
+        public InformationDocumentController(IMapper mapper, ILoggerFactory loggerFactory)
+        {
+            this.mapper = mapper;
+            this.logger = loggerFactory.CreateLogger("InformationDocument info");
+        }
+
+        [HttpGet("{page?}/{count?}")]
+        public async Task<ActionResult<InformationDocumentDTO>> GetAll(int page = 0, int count = 10)
+        {
+            page = page > 0 ? page - 1 : 0;
+            count = count > 50 ? 50 : count;
+
+            var position = count * page;
+
+            var query = new GetInformationDocumentListQuery
+            {
+                Position = position,
+                Take = count
+            };
+
+            var result = await Mediator.Send(query);
+
+            return Ok(result);
+        }
+    }
+}
