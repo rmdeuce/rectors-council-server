e97b1b1487b62f6ae3faf0522f76d2383507ff87
rmdeuce <jlutkin.rv@tgmu.ru>
2024-03-22 14:10:12 +1000

    [RCS-2634] реализована загрузка фотографий для Universities через CreateUniversityFilePathCommand, добавление IconUrl путем выбора из существующих фотограйи в PhotosUrl через CreateUniversityIconUrlCommand, добавлен новый exception - NotContainsFileException

---
+++ Application/Features/Universities/Commands/CreateUniversityIconUrlCommand.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Universities.Commands
+{
+    public class CreateUniversityIconUrlCommand : IRequest<string>
+    {
+        public string FilePath { get; set; }
+        public int UniversityId { get; set; }
+
+        public CreateUniversityIconUrlCommand(string filePath, int universityId)
+        {
+            this.FilePath = filePath;
+            this.UniversityId = universityId;
+        }
+    }
+}
---
+++ Application/Features/Universities/Commands/CreateUniversityIconUrlCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using Application.Common.Exceptions;
+using Application.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Universities.Commands
+{
+    public class CreateUniversityIconUrlCommandHandler : IRequestHandler<CreateUniversityIconUrlCommand, string>
+    {
+        private readonly IAppDBContext dbContext;
+        private readonly IMediator mediator;
+
+        public CreateUniversityIconUrlCommandHandler(IAppDBContext dbcontext, IMediator mediator)
+        {
+            this.dbContext = dbcontext;
+            this.mediator = mediator;
+        }
+
+        public async Task<string> Handle(CreateUniversityIconUrlCommand request, CancellationToken cancellationToken)
+        {
+            var filePath = request.FilePath;
+            var universityId = request.UniversityId;
+
+            var entity = await dbContext.Universities.FindAsync(new object[] { universityId }, cancellationToken);
+
+            if (entity == null)
+                throw new NotFoundException(nameof(Domain.Entities.University), request.UniversityId);
+
+            if (!entity.PhotosUrl.Contains(filePath))
+                throw new NotContainsFileException(filePath, nameof(Domain.Entities.University));
+
+            entity.IconUrl = filePath;
+
+            await dbContext.SaveChangesAsync(cancellationToken);
+
+            return filePath;
+        }
+    }
+}
